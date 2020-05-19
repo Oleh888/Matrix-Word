6e845fcc49b18fa85414d7cf0d5a15ff9d0e712d
Oleh888 <60585035+Oleh888@users.noreply.github.com>
2020-05-19 16:03:53 +0300

    Update src/main/java/console/app/Main.java
    Co-authored-by: Sofia Diakonova <36365171+Sofasmile@users.noreply.github.com>

--- src/main/java/console/app/Main.java
+++ src/main/java/console/app/Main.java
@@ -2,7 +2,10 @@ package console.app;
 
 public class Main {
     public static void main(String[] args) {
+        ConsoleHandler consoleHandler = new ConsoleHandler();
+        String sequence = consoleHandler.enterSequence();
+        String word = consoleHandler. enterWord();
         MatrixService matrix = new MatrixService();
-        System.out.println("result sequence of cells is " + matrix.getCharsSequence());
+        System.out.println("result sequence of cells is " + matrix.getCharsSequence(sequence, word));
     }
 }
